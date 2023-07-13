bf53c67649c1c3327228f2358d8cb399e4c65281
aritz roca <arocapazos@gmail.com>
2023-07-13 16:10:14 +0200

    cambio a carpeta todo el curso

---
+++ Basic/11_clases.py
@@ -0,0 +1,52 @@
+### Clases ###
+
+""" Que son las clases??? Las clases nos otorgan un principio y un fin un objeto"""
+
+class MyEmptyPerson: # Como buena practica las clases las escribiremos con el formato camel
+    pass # Este comando hace que si esta clase no tine nada no de error, solo tine sentido a la hora de definir la clase
+
+print(MyEmptyPerson)
+print(MyEmptyPerson())
+
+class EmptyPerson:
+    def __init__(self,) -> None: # _init es un constructor de clase, no una funcion, en este caso no esta haciendo nada
+        pass
+
+print("\nEjemplo 1 una sola clase con dos claves")
+class Person:
+    def __init__(self, name, surname):
+        self.name = name # Con el self.creamos la propiedad dentro de nuestra clase u le asignamos el parametro que queramos
+        self.surname = surname
+        #pass #Quitamos el pass para que funcione
+
+my_person = Person("Roca", "Aritz")
+print(my_person.name)
+print(f"{my_person.name} {my_person.surname}")
+
+
+print("\nEjemplo 2 una sola clase con una clave")
+class Person:
+    def __init__(self, name, surname):
+        self.ful_name = f"{name} {surname}"
+
+my_person = Person("Aritz", "Roca")
+print(my_person.ful_name)
+
+print("\nEjemplo 3 funcion dentro de la clase")
+""" Una funcion dentro de una clase no tiene que llamar al nombre de su clase sino a self para acceder atodo lo que este guardado ya que ya esta dentro"""
+class Person:
+    def __init__(self, name, surname, alias = ""):
+        self.full_name = f"{name} {surname} {alias}"
+    
+    def walk (self):
+        print(f"{self.full_name} Esta caminando")
+
+my_person = Person("Aritz", "Roca", "aroca-pa")
+print(my_person.full_name)
+my_person.walk ()
+
+print("\nejemplo 3.1 sin alias y cambiando el valor")
+my_other_person = Person("Aritz", "Roca")
+my_other_person.full_name = "Hector de Leon (El loco de los perros)" # Vemos que podemos cambiar el valor independientemente de lo que le allamos determinado al constructor de clase
+print (my_other_person.full_name)
+
